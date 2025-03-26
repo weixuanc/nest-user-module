b187390d0aeb0fcb89b0138af4df4c11ffa02e32
chenweixuan <wxchen@energy2iot.com>
2025-03-26 15:54:16 +0800

    新增数据库表

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'login_test',
+      synchronize: true,
+      logging: true,
+      entities: [User],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugins: 'sha256_password'
+      }
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
